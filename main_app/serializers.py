524f9260773333f5fbc6914fe6b70fdf22e16075
Raul Jiminian <raul.e.jiminian@gmail.com>
2025-05-06 13:32:02 -0400

    many to many

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Cat, Feeding, Toy
 
+class ToySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Toy
+    fields = '__all__'
+
+
 class CatSerializer(serializers.ModelSerializer):
   fed_for_today = serializers.SerializerMethodField()
+  toys = ToySerializer(many=True, read_only=True) #add this line
   
   class Meta:
     model = Cat
@@ -19,7 +26,3 @@ class FeedingSerializer(serializers.ModelSerializer):
     read_only_fields = ('cat',)
 
 
-class ToySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Toy
-    fields = '__all__'
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail, ToyList, ToyDetail
+from .views import (
+  Home, 
+  CatList, 
+  CatDetail, 
+  FeedingListCreate, 
+  FeedingDetail, 
+  ToyList, 
+  ToyDetail, 
+  AddToyToCat,
+  RemoveToyFromCat
+)
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
@@ -8,5 +18,7 @@ urlpatterns = [
   path('cats/<int:cat_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
 	path('cats/<int:cat_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
   path('toys/', ToyList.as_view(), name='toy-list'),
-  path('toys/<int:id>', ToyDetail.as_view(), name='toy-detail')
+  path('toys/<int:id>', ToyDetail.as_view(), name='toy-detail'),
+  path('cats/<int:cat_id>/add_toy/<int:toy_id>/', AddToyToCat.as_view(), name='add-toy-to-cat'),
+  path('cats/<int:cat_id>/remove_toy/<int:toy_id>/', RemoveToyFromCat.as_view(), name='remote-toy-from-cat')
 ]
